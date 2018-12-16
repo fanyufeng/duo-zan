f76cea7b39d4310e7e87f2ff2e04f4e26bd2baa7
fanyufeng <fyf820@163.com>
2018-12-16 13:17:41 +0800

    update

--- src/main/java/com/duode/config/UploadConfiguration.java
+++ src/main/java/com/duode/config/UploadConfiguration.java
@@ -18,8 +18,8 @@ public class UploadConfiguration extends WebMvcConfigurerAdapter {
         //上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/d3cf0281-bb7f-40e0-ab77-406db95ccf2c.jpg
         //其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
         //registry.addResourceHandler("/**").addResourceLocations("/duode/dituo/dituo/document/");
-        registry.addResourceHandler("/images/**").addResourceLocations("file:/duode/dituo/dituo/images/");
-        registry.addResourceHandler("/video/**").addResourceLocations("file:/duode/dituo/dituo/video");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:/duode/dituo/images/");
+        registry.addResourceHandler("/video/**").addResourceLocations("file:/duode/dituo/video");
     }
 }
 
