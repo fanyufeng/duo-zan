15696f082b5db45d1f1d2daf79038e6f5ce6e7dd
fanyufeng <fyf820@163.com>
2018-12-29 23:42:53 +0800

    update

--- src/main/resources/create_table/create_cash_table.sql
+++ src/main/resources/create_table/create_cash_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `cash`(
    `id` INT UNSIGNED AUTO_INCREMENT,
-   `user_id` VARCHAR(100) '交易人id',
-   `cash_elem` double not null DEFAULT 0 comment '交易金额',
-   `comment` VARCHAR(100) '备注',
+   `user_id` VARCHAR(100) comment '交易人id',
+   `cash_elem` double comment '交易金额',
+   `comment` VARCHAR(100) comment '备注',
    `create_time` DATETIME,
    `modify_time` DATETIME,
    PRIMARY KEY ( `id` )
