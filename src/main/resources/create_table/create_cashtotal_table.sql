5acc87a26bc719e7af3b1544d0ae6fa9a7390890
fanyufeng <fyf820@163.com>
2019-01-21 22:57:12 +0800

    update

--- src/main/resources/create_table/create_cashtotal_table.sql
+++ src/main/resources/create_table/create_cashtotal_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `cash_total`(
    `id` INT UNSIGNED AUTO_INCREMENT,
    `cash_total` double comment '金额总量',
+   `name` varchar(100) comment '分配名称',
    `comment` varchar(100) comment '备注',
    `status` int default 0 comment '0 表示正常，1表示已分配完',
    `user_id` varchar(100) comment '上传人',
