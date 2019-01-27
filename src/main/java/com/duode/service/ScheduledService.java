028c870b3c4e07cc6a39171a8658f516bbcfaf20
fanyufeng <fyf820@163.com>
2019-01-27 17:57:26 +0800

    update

--- src/main/java/com/duode/service/ScheduledService.java
+++ src/main/java/com/duode/service/ScheduledService.java
@@ -31,7 +31,7 @@ public class ScheduledService {
 
     SimpleDateFormat sdft = new SimpleDateFormat("yyyy-MM-dd");
 
-    @Scheduled(cron = "0 57 17 * * ?")
+    @Scheduled(cron = "0 0 18 * * ?")
     public void cashDistribute () {
         try {
             List<User> userList = userService.getUserList();
